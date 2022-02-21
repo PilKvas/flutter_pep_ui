ba5105b7d21829814085fe35922c9b61d1bf2587
PilKvas <hm10bvb@mail.ru>
2022-02-21 11:14:11 +0300

    new Ui

---
+++ lib/configuration.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+
+Color primaryGreen = Color(0xff416d6d);
+Color lightGrey = Color(0xFFF5F5F5);
+List<BoxShadow> shadowList = [
+  BoxShadow(color: lightGrey, blurRadius: 100, offset: Offset(0, 10))
+];
+
+List<Map> categories = [
+  {'name': 'Cats', 'iconPath': 'images/cat.png'},
+  {'name': 'Dogs', 'iconPath': 'images/dog.png'},
+  {'name': 'Bunnies', 'iconPath': 'images/rabbit.png'},
+  {'name': 'Parrots', 'iconPath': 'images/parrot.png'},
+  {'name': 'Horses', 'iconPath': 'images/horse.png'}
+];
+
+List<Map> drawerItems=[
+  {
+    'icon': FontAwesomeIcons.paw,
+    'title' : 'Adoption'
+  },
+  {
+    'icon': Icons.mail,
+    'title' : 'Donation'
+  },
+  {
+    'icon': FontAwesomeIcons.plus,
+    'title' : 'Add pet'
+  },
+  {
+    'icon': Icons.favorite,
+    'title' : 'Favorites'
+  },
+  {
+    'icon': Icons.mail,
+    'title' : 'Messages'
+  },
+  {
+    'icon': FontAwesomeIcons.userAlt,
+    'title' : 'Profile'
+  },
+];
+
+mixin FontAwesomeIcons {
+  static var paw;
+
+  static var plus;
+
+  static var userAlt;
+}
